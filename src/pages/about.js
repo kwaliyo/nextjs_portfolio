14b7ff1410b1df8cff0801306110b2fc1bb52020
Muhammad Jaafar <65981875+kwaliyo@users.noreply.github.com>
2024-09-24 14:43:46 +0100

    Update about.js
    Commented the image and change the years of experience

--- src/pages/about.js
+++ src/pages/about.js
@@ -74,23 +74,23 @@ const about = () => {
               </p>
             </div>
 
-            <div className=" xl:col-span-4 md:col-span-8 md:order-1 col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light">
-              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
-              <Image
-                src={pp2}
-                alt="MKJ"
-                className="w-full h-auto rounded-2xl"
-                priority
-                sizes="(max-width: 768px) 100vw,
-              (max-width: 1200px) 50vw,
-              33vw"
-              />
-            </div>
+            // <div className=" xl:col-span-4 md:col-span-8 md:order-1 col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light">
+            //   <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
+            //   <Image
+            //     src={pp2}
+            //     alt="MKJ"
+            //     className="w-full h-auto rounded-2xl"
+            //     priority
+            //     sizes="(max-width: 768px) 100vw,
+            //   (max-width: 1200px) 50vw,
+            //   33vw"
+            //   />
+            // </div>
 
             <div className=" xl:col-span-8 xl:flex-row xl:items-center md:order-3 col-span-2 flex flex-col items-end justify-between">
               <div className="flex flex-col items-end justify-center xl:items-center">
                 <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                  <AnimatedNumbers value={50} />+
+                  <AnimatedNumbers value={5} />+
                 </span>
                 <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
                   Satisfied Clients
@@ -108,7 +108,7 @@ const about = () => {
 
               <div className="flex flex-col items-end justify-center xl:items-center">
                 <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                  <AnimatedNumbers value={50} />+
+                  <AnimatedNumbers value={20} />+
                 </span>
                 <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75  xl:text-center md:text-lg sm:text-base xs:text-sm">
                   Years of Experience
