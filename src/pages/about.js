aada51980ca8ebbf747581254d8ef215237d433f
Muhammad Jaafar <65981875+kwaliyo@users.noreply.github.com>
2024-09-24 14:58:34 +0100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -74,19 +74,6 @@ const about = () => {
               </p>
             </div>
 
-            // <div className=" xl:col-span-4 md:col-span-8 md:order-1 col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light">
-            //   <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
-            //   <Image
-            //     src={pp2}
-            //     alt="MKJ"
-            //     className="w-full h-auto rounded-2xl"
-            //     priority
-            //     sizes="(max-width: 768px) 100vw,
-            //   (max-width: 1200px) 50vw,
-            //   33vw"
-            //   />
-             //     </div>
-            // </div>
 
             <div className=" xl:col-span-8 xl:flex-row xl:items-center md:order-3 col-span-2 flex flex-col items-end justify-between">
               <div className="flex flex-col items-end justify-center xl:items-center">
